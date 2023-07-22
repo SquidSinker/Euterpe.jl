0e6742ecbbb5e4b9905fa1a40f49f11225be4b88
SquidSinker <squidsinker@users.noreply.github.com>
2023-07-22 14:48:49 -0400

    fix compressor

--- src/effects.jl
+++ src/effects.jl
@@ -1,8 +1,11 @@
 function compress(sound::Vector, threshold::Float64, ratio::Float64)
     compressed = []
     for i in sound
-        if i > threshold
+        if abs(i) > threshold
             push!(compressed, i/ratio)
+        else 
+            push!(compressed, i)
         end
     end
+    return compressed
 end
