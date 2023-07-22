a36e1fc5209091b180ec9cb10c4ebfe8a3ca3a66
SquidSinker <squidsinker@users.noreply.github.com>
2023-07-22 17:59:05 -0400

    Add lowpass filter

--- src/effects.jl
+++ src/effects.jl
@@ -21,3 +21,15 @@ function clip_dist(sound::Vector, threshold::Float64)
     end
     return dist
 end
+
+function lowpass(sound::Vector, amount::Float64)
+    # most effective at amount = 0.5
+    if amount > 1 || amount < 0
+        error("amount must be between 0 and 1")
+    end
+    filtered = [sound[1]]
+    for i in 2:length(sound)
+        push!(filtered, amount * sound[i] + (1-amount) * sound[i-1])
+    end
+    return
+end
