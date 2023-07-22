7369510d247ea5c5d02953369f7e2db6078260d4
SquidSinker <squidsinker@users.noreply.github.com>
2023-07-22 17:33:05 -0400

    clip distortion

--- src/effects.jl
+++ src/effects.jl
@@ -3,9 +3,21 @@ function compress(sound::Vector, threshold::Float64, ratio::Float64)
     for i in sound
         if abs(i) > threshold
             push!(compressed, i/ratio)
-        else 
+        else
             push!(compressed, i)
         end
     end
     return compressed
+end
+
+function clip_dist(sound::Vector, threshold::Float64)
+    dist = []
+    for i in sound
+        if abs(i) > threshold
+            push!(dist, threshold)
+        else
+            push!(dist, i)
+        end
+    end
+    return dist
 end
